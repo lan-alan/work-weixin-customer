77807fe6b8e18fc73e81ee5ce5f72904f8e4bb2f
Alan <lanhonghui100@gmail.com>
2022-03-10 23:55:21 +0800

    创建项目

---
+++ src/main/java/com/alanjgg/lmjg/WorkWeixinCustomerApplication.java
@@ -0,0 +1,13 @@
+package com.alanjgg.lmjg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkWeixinCustomerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorkWeixinCustomerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/alanjgg/lmjg/WorkWeixinCustomerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alanjgg.lmjg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkWeixinCustomerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
