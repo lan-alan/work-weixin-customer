192ffd993d479dbf4db1a3f904466c9ea41bd2be
Alan <lanhonghui100@gmail.com>
2022-06-26 10:34:45 +0800

    获取小程序scheme

--- src/test/java/com/alanjgg/wwc/WorkWeixinCustomerApplicationTests.java
+++ src/test/java/com/alanjgg/wwc/WorkWeixinCustomerApplicationTests.java
@@ -1,5 +1,6 @@
 package com.alanjgg.wwc;
 
+import cn.hutool.http.HttpRequest;
 import com.alanjgg.wwc.api.RequestApi;
 import com.alanjgg.wwc.common.Constants;
 import com.alanjgg.wwc.service.WxKeFuInfoService;
@@ -7,17 +8,29 @@ import com.alanjgg.wwc.utils.HttpUtil;
 import com.alibaba.fastjson.JSONObject;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
+import org.apache.http.Header;
+import org.apache.http.HttpHeaders;
+import org.apache.http.client.methods.HttpUriRequest;
+import org.apache.http.client.methods.RequestBuilder;
 import org.apache.http.client.utils.DateUtils;
+import org.apache.http.entity.ContentType;
+import org.apache.http.message.BasicHeader;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import weixin.popular.bean.BaseResult;
+import weixin.popular.client.LocalHttpClient;
 
 import java.io.File;
+import java.io.IOException;
+import java.net.URLDecoder;
 import java.util.*;
 
 @Slf4j
 @SpringBootTest
 class WorkWeixinCustomerApplicationTests {
+
+    public static Header JSON_HEADER = new BasicHeader(HttpHeaders.CONTENT_TYPE, ContentType.APPLICATION_JSON.toString());
 
     @Autowired
     private RequestApi requestApi;
@@ -85,6 +98,82 @@ class WorkWeixinCustomerApplicationTests {
             String kfId = map.get("open_kfid").toString();
             int count = wxKeFuInfoService.saveWxKeFuInfo(name, kfId);
             log.info("count = {}", count);
+        }
+    }
+
+    @Test
+    public void getWeiXinAccessTokenTest() {
+
+        // https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=APPID&secret=APPSECRET
+
+//        String uri = "https://api.weixin.qq.com/cgi-bin/token";
+//
+//        HttpUriRequest httpUriRequest = RequestBuilder.post()
+//                .setHeader(JSON_HEADER)
+//                .setUri(uri)
+//                .addParameter("grant_type", "client_credential")
+////                .addParameter("appid", "wx5b9dcf34bbde474c")
+//                .addParameter("appid", "wx424eeae2a0648a4f")
+//                .addParameter("secret", "52809ee0c5233c1f56791200567e81b9")
+//                .build();
+//
+//        BaseResult result = LocalHttpClient.executeJsonResult(httpUriRequest, BaseResult.class);
+//
+//        System.out.println("result = " + JSONObject.toJSONString(result));
+
+
+        String url = "https://api.weixin.qq.com/cgi-bin/token";
+        Map<String, String> paramMap = new HashMap<>();
+        paramMap.put("grant_type", "client_credential");
+        paramMap.put("appid", "wx424eeae2a0648a4f");
+        paramMap.put("secret", "52809ee0c5233c1f56791200567e81b9");
+        String result = HttpUtil.doGet(url, paramMap);
+        // {"access_token":"58_ycXNolFMtsKj60MFi55_5De2LzBjfnblauR03lYSU518tWT7_6Ac-FIjvVKOQkP7h09kE8lY0s2rlYFcENlNnEHDoMEw9M3OceLB-MMLDQtJM0IP6l_UIQg7otO9rbQ1rFR26PW4Uctos3x0LQFcAHAZET","expires_in":7200}
+        // token = "58_ycXNolFMtsKj60MFi55_5De2LzBjfnblauR03lYSU518tWT7_6Ac-FIjvVKOQkP7h09kE8lY0s2rlYFcENlNnEHDoMEw9M3OceLB-MMLDQtJM0IP6l_UIQg7otO9rbQ1rFR26PW4Uctos3x0LQFcAHAZET"
+        log.info("result = {}", result);
+
+    }
+
+    @Test
+    public void generateschemeTest() {
+        // POST https://api.weixin.qq.com/wxa/generatescheme?access_token=ACCESS_TOKEN
+
+        String accessToken = "58_ycXNolFMtsKj60MFi55_5De2LzBjfnblauR03lYSU518tWT7_6Ac-FIjvVKOQkP7h09kE8lY0s2rlYFcENlNnEHDoMEw9M3OceLB-MMLDQtJM0IP6l_UIQg7otO9rbQ1rFR26PW4Uctos3x0LQFcAHAZET";
+        String url = "https://api.weixin.qq.com/wxa/generatescheme?access_token=" + accessToken;
+        Map<String, String> paramMap = new HashMap<>();
+        JSONObject jumpWxa = new JSONObject();
+        jumpWxa.put("path", "/pages/home/home");
+        jumpWxa.put("query", "");
+        paramMap.put("jump_wxa", jumpWxa.toJSONString());
+        paramMap.put("expire_type", "1");
+        paramMap.put("expire_interval", "30");
+        String result = HttpUtil.doPost(url, paramMap);
+        log.info("result = {}", result);
+
+    }
+
+    @Test
+    public void getOpenLinkTest() {
+        String token = "58_ycXNolFMtsKj60MFi55_5De2LzBjfnblauR03lYSU518tWT7_6Ac-FIjvVKOQkP7h09kE8lY0s2rlYFcENlNnEHDoMEw9M3OceLB-MMLDQtJM0IP6l_UIQg7otO9rbQ1rFR26PW4Uctos3x0LQFcAHAZET";
+        String url = "https://api.weixin.qq.com/wxa/generatescheme?access_token=" + token;
+        JSONObject jumpWxa = new JSONObject();
+        jumpWxa.put("path", "/pages/home/home");
+        jumpWxa.put("query", "");
+
+        JSONObject reqMap = new JSONObject();
+        reqMap.put("jump_wxa", jumpWxa);
+        reqMap.put("expire_type", 1);
+        reqMap.put("expire_interval", 30);
+        String result = "";
+        try {
+            HttpRequest request = cn.hutool.http.HttpUtil.createPost(url);
+            request.contentType("application/json");
+            request.body(reqMap.toJSONString());
+            result = request.execute().body();
+            result = URLDecoder.decode(result, "UTF-8");
+            System.out.print("微信获取getOpenLink回调报文" + result);
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 
